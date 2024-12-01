93b8ef95f08339542dc31d61ddc50a6f76144efc
nunzioono <nunzioonorati@gmail.com>
2024-12-01 18:10:46 +0000

    changed serving options

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -38,7 +38,7 @@ async fn start() -> Result<(), rocket::Error> {
 }
 
 fn cors() -> Cors {
-    let allowed_origins = AllowedOrigins::all();
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:8000", "http://127.0.0.1:8000"]);
     rocket_cors::CorsOptions {
         allowed_origins,
         allowed_methods: vec![Method::Get, Method::Post, Method::Delete]
