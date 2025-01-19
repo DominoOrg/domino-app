f89328597e773499589bfbeaadb16ac0ced43306
nunzioono <nunzioonorati@gmail.com>
2025-01-19 12:51:10 +0100

    Fixed tile mapping when retriving inserted tiles from db

--- backend/src/db/query.rs
+++ backend/src/db/query.rs
@@ -1,5 +1,3 @@
-use std::fmt::Error;
-
 use domino_lib::types::{Puzzle, Tile};
 use rand::prelude::*;
 
@@ -11,7 +9,7 @@ pub fn query_puzzle(n: usize, c: usize) -> Result<Puzzle, sqlite::Error> {
     let mut puzzle = Puzzle::default();
     let connection = open_connection()?;
     let mut stmt = "
-        SELECT P.id
+        SELECT P.id, C.len
         FROM puzzle P, collection C
         WHERE n = ".to_owned() + &n.to_string() + " 
         AND c = " + &c.to_string() + " 
@@ -19,6 +17,7 @@ pub fn query_puzzle(n: usize, c: usize) -> Result<Puzzle, sqlite::Error> {
     ";
     println!("stmt: {}", stmt);
     let mut valid_puzzle_ids = vec![];
+    let mut valid_puzzle_lengths = vec![];
     connection.iterate(stmt, |result| {
         for (column, value) in result {
             if column.to_string() == "id".to_string() {
@@ -26,17 +25,20 @@ pub fn query_puzzle(n: usize, c: usize) -> Result<Puzzle, sqlite::Error> {
                     let puzzle_id = value.to_string();                    
                     valid_puzzle_ids.push(puzzle_id);
                 }
+            } else if column.to_string() == "len".to_string() {
+                if let Some(value) = value {
+                    let puzzle_length = value.to_string().parse::<i32>().unwrap();
+                    valid_puzzle_lengths.push(puzzle_length);
+                }
             }
         }
         true
     }).expect("Error fetching the puzzle id");
 
-    if valid_puzzle_ids.len() == 0 {
-        return Ok(vec![]);
-    }
     let mut rand_seed = rand::thread_rng();
     let rand_index = rand_seed.gen_range(0..valid_puzzle_ids.len());
     let puzzle_id = valid_puzzle_ids[rand_index].clone();
+    let puzzle_len = valid_puzzle_lengths[rand_index];
     let mut tiles_info: Vec<(String, String)> = vec![];
     stmt = "SELECT tile_id, position FROM inserted_tile WHERE collection_id = \"".to_owned() + &puzzle_id +"\"";
     println!("stmt: {}", stmt);
@@ -55,34 +57,40 @@ pub fn query_puzzle(n: usize, c: usize) -> Result<Puzzle, sqlite::Error> {
     }).expect("Error fetching inserted tiles");
 
     let mut tiles: Vec<Option<Tile>> = vec![];
-    for tile_info in tiles_info {
-        stmt = "SELECT left, right FROM tile WHERE id = \"".to_owned() + &tile_info.0 +"\"";
-        println!("stmt: {}", stmt);
-        connection.iterate(stmt, |result| {
-            let mut left = -1;
-            let mut right = -1;
-
-            for (column, value) in result {
-
-                if column.to_string() == "left".to_string() {
-                    left = value.unwrap().parse::<i32>().unwrap();
-                    if right != -1 {
-                        let tile = Tile(left, right);
-                        tiles.push(Some(tile));
-                        return true;
-                    }
-                } else if column.to_string() == "right".to_string() {
-                    right = value.unwrap().parse::<i32>().unwrap();
-                    if left != -1 {
-                        let tile = Tile(left, right);
-                        tiles.push(Some(tile));
-                        return true;
+    for tile_index in 0..puzzle_len {
+        let tile_info = tiles_info.iter().find(|tile_info| tile_info.1 == tile_index.to_string());
+        if let Some(tile_info) = tile_info {
+            stmt = "SELECT left, right FROM tile WHERE id = \"".to_owned() + &tile_info.0 +"\"";
+            println!("stmt: {}", stmt);
+            connection.iterate(stmt, |result| {
+                let mut left = -1;
+                let mut right = -1;
+    
+                for (column, value) in result {
+    
+                    if column.to_string() == "left".to_string() {
+                        left = value.unwrap().parse::<i32>().unwrap();
+                        if right != -1 {
+                            let tile = Tile(left, right);
+                            tiles.push(Some(tile));
+                            return true;
+                        }
+                    } else if column.to_string() == "right".to_string() {
+                        right = value.unwrap().parse::<i32>().unwrap();
+                        if left != -1 {
+                            let tile = Tile(left, right);
+                            tiles.push(Some(tile));
+                            return true;
+                        }
                     }
                 }
-            }
-
-            false
-        }).expect("Error fetching the tiles");
+    
+                false
+            }).expect("Error fetching the tiles");    
+        } else {
+            tiles.push(None);
+            continue;
+        }
     }
     puzzle.extend_from_slice(tiles.as_slice());
     Ok(puzzle)
--- backend/src/endpoints/mod.rs
+++ backend/src/endpoints/mod.rs
@@ -1,25 +1,27 @@
-use rocket::{get, serde::json::Json};
+use rocket::{get, http::Status, serde::json::Json, Data};
 use crate::db::select_puzzle_from_db;
 
 type ApiPuzzle = Vec<Option<Vec<i32>>>;
 
 #[get("/select_puzzle?<n>&<c>")]
-pub async fn select_puzzle(n: i32, c: i32) -> Json<ApiPuzzle> {
+pub async fn select_puzzle(n: i32, c: i32) -> Result<Json<ApiPuzzle>,Status> {
     println!("n: {}, c: {}", n, c);
     if let Ok(puzzle) = select_puzzle_from_db(n, 0) {
-        Json(
-            puzzle
-            .into_iter()
-            .map(|option_tile|
-                if let Some(tile) = option_tile {
-                    Some(vec![tile.0, tile.1])
-                } else {
-                    None
-                }
-            )
-            .collect()
+        println!("puzzle: {:?}", puzzle);
+        let mapped_puzzle = puzzle
+        .into_iter()
+        .map(|option_tile|
+            if let Some(tile) = option_tile {
+                Some(vec![tile.0, tile.1])
+            } else {
+                None
+            }
         )
+        .collect();
+        println!("mapped_puzzle: {:?}", mapped_puzzle);
+        let json_puzzle = Json(mapped_puzzle);
+        Ok(json_puzzle)
     } else {
-        Json(vec![])
+        Err(Status { code: 404 })
     }
 }
