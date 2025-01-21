de2120061b51a6bfc312f107a1da4e74c82c9684
nunzioono <nunzioonorati@gmail.com>
2025-01-21 22:35:53 +0100

    Fixed test feature passing c=0 for all api calls

--- backend/src/endpoints/mod.rs
+++ backend/src/endpoints/mod.rs
@@ -7,7 +7,7 @@ type ApiPuzzle = Vec<Option<Vec<i32>>>;
 #[get("/select_puzzle?<n>&<c>")]
 pub fn select_puzzle(n: i32, c: i32) -> Result<Json<ApiPuzzle>,Status> {
     println!("n: {}, c: {}", n, c);
-    if let Ok(puzzle) = select_puzzle_from_db(n, 0) {
+    if let Ok(puzzle) = select_puzzle_from_db(n, c) {
         println!("puzzle: {:?}", puzzle);
         let mapped_puzzle = puzzle
         .into_iter()
