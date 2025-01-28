bc4849fff1c49bcc04b339ee70582205352cfbdb
nunzioono <nunzioonorati@gmail.com>
2025-01-28 20:14:13 +0100

    Removed unused dependency in dialog component

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { cn } from "@/utils/css"
-import { Cross2Icon } from "@radix-ui/react-icons"
 
 const Dialog = DialogPrimitive.Root
 
