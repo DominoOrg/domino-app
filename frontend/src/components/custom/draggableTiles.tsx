3f637da3567d21c7068ff2b261054e79801c3d94
nunzioono <nunzioonorati@gmail.com>
2025-02-03 17:43:22 +0100

    Updated import from useSelectPuzzle to usePuzzle

--- frontend/src/components/custom/draggableTiles.tsx
+++ frontend/src/components/custom/draggableTiles.tsx
@@ -1,6 +1,6 @@
 import { computeTileset, filterTiles } from "@/utils/tileset";
 import Tile from "./tile/tile";
-import { Puzzle } from "@/hooks/api/useSelectPuzzle";
+import { Puzzle } from "@/hooks/api/usePuzzle";
 
 interface RemainingTilesProps {
   puzzle: Puzzle,
