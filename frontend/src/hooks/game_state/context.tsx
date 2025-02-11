d9d945643ca08d5e2d08f2d05d160be74e5d820c
nunzioono <nunzioonorati@gmail.com>
2025-02-11 10:52:32 +0100

    refactor: Simplify moveTile logic and integrate it into action payload for better state management

--- frontend/src/hooks/game_state/context.tsx
+++ frontend/src/hooks/game_state/context.tsx
@@ -18,20 +18,6 @@ export interface GameContextInteface {
     moveTile: (tile: Option<Tile>, at: number) => void;
 }
 
-type Action = {
-    type: "MOVE_TILE",
-    payload: {
-        tile: Option<Tile>,
-        at: number
-    }
-} | {
-    type: "UPDATE_DRAGGING",
-    payload: Option<{
-        tile: Option<Tile>,
-        id: number
-    }>
-};
-
 export const GameContext = React.createContext<GameContextInteface | undefined>(undefined);
 
 export const GameContextProvider = ({ puzzle, children }: { puzzle: Array<Option<Tile>>, children: ReactNode }) => {
@@ -47,36 +33,9 @@ export const GameContextProvider = ({ puzzle, children }: { puzzle: Array<Option
         tileset,  // Ensure TileSet can be initialized with an empty array
         isDragging: null
     };
-    const reducer = (prevState: GameContextInteface, action: Action): GameContextInteface => {
-        switch (action.type) {
-            case "UPDATE_DRAGGING":
-                let newState = { ...prevState };
-                newState.state.isDragging = action.payload;
-                return newState;
-            case "MOVE_TILE":
-                const { tile, at }: { tile: Option<Tile>, at: number } = action.payload;
-                const { inBoardTiles, freeTiles, insertedTiles }: {
-                    inBoardTiles: Array<Option<Tile>>,
-                    freeTiles: Array<Option<Tile>>,
-                    insertedTiles: Array<[Option<Tile>, number]>
-                } = prevState.state;
-                const newInBoardTiles: Array<Option<Tile>> = [...inBoardTiles];
-                const tmp = newInBoardTiles[at];
-                newInBoardTiles[at] = tile;
-                const newInsertedTiles: Array<[Option<Tile>, number]> = [...insertedTiles, [tile, at]];
-                const newFreeTiles: Array<Option<Tile>> = [...freeTiles, tmp];
-                const newGameState = {
-                    ...prevState.state,
-                    inBoardTiles: newInBoardTiles,
-                    freeTiles: newFreeTiles,
-                    insertedTiles: newInsertedTiles
-                };
-                return { state: newGameState, moveTile };
-        }
-    };
-
+    
     const moveTile = (tile: Option<Tile>, at: number) => {
-        dispatch({ type: "MOVE_TILE", payload: { tile, at } });
+        dispatch({ type: "MOVE_TILE", payload: { tile, at, moveTile } });
     };
 
     const [{state}, dispatch] = useReducer(reducer, {state: initialState, moveTile});
@@ -91,3 +50,45 @@ export const GameContextProvider = ({ puzzle, children }: { puzzle: Array<Option
     );
 };
 
+type Action = {
+    type: "MOVE_TILE",
+    payload: {
+        tile: Option<Tile>,
+        at: number,
+        moveTile: (tile: Option<Tile>, at: number) => void
+    }
+} | {
+    type: "UPDATE_DRAGGING",
+    payload: Option<{
+        tile: Option<Tile>,
+        id: number
+    }>
+};
+
+const reducer = (prevState: GameContextInteface, action: Action): GameContextInteface => {
+    switch (action.type) {
+        case "UPDATE_DRAGGING":
+            let newState = { ...prevState };
+            newState.state.isDragging = action.payload;
+            return newState;
+        case "MOVE_TILE":
+            const { tile, at, moveTile }: { tile: Option<Tile>, at: number, moveTile: (tile: Option<Tile>, at: number) => void } = action.payload;
+            const { inBoardTiles, freeTiles, insertedTiles }: {
+                inBoardTiles: Array<Option<Tile>>,
+                freeTiles: Array<Option<Tile>>,
+                insertedTiles: Array<[Option<Tile>, number]>
+            } = prevState.state;
+            const newInBoardTiles: Array<Option<Tile>> = [...inBoardTiles];
+            const tmp = newInBoardTiles[at];
+            newInBoardTiles[at] = tile;
+            const newInsertedTiles: Array<[Option<Tile>, number]> = [...insertedTiles, [tile, at]];
+            const newFreeTiles: Array<Option<Tile>> = [...freeTiles, tmp];
+            const newGameState = {
+                ...prevState.state,
+                inBoardTiles: newInBoardTiles,
+                freeTiles: newFreeTiles,
+                insertedTiles: newInsertedTiles
+            };
+            return { state: newGameState, moveTile };
+    }
+};
