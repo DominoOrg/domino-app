3420f3e898fcf149752c5da4af48e329df65554b
nunzioono <nunzioonorati@gmail.com>
2025-02-18 21:33:00 +0100

    Fix: Removed more linting warnings

--- frontend/src/hooks/game_state/context.tsx
+++ frontend/src/hooks/game_state/context.tsx
@@ -1,6 +1,5 @@
-import React, { ReactNode, useReducer } from "react";
+import React from "react";
 import { Tile, Option, TileSet } from "./types";
-import { reducer } from "./reducer";
 
 export interface GameState {
     inBoardTiles: Array<Option<Tile>>,
@@ -13,37 +12,3 @@ export interface GameState {
 
 export const GameContext = React.createContext<GameState | undefined>(undefined);
 
-export const GameContextProvider = ({ puzzle, children }: { puzzle: Array<Option<Tile>>, children: ReactNode }) => {
-    const tileset = new TileSet(puzzle);
-    const freeTiles = tileset.iter()
-        .filter(tile =>
-            !puzzle.some(t => t && t.is_equal(tile))
-        );
-
-    const initialState: GameState = {
-        inBoardTiles: puzzle,
-        insertedPositions: [],
-        freeTiles,
-        tileset,
-        moveTile: () => {},
-        rotateTile: () => {},
-    };
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const moveTile = (from: number, to: number) => {
-        dispatch({ type: "MOVE_TILE", payload: { from, to } });
-    };
-
-    const rotateTile = (index: number) => {
-        dispatch({ type: "ROTATE_TILE", payload: { index } });
-    };
-
-    const updatedState: GameState = { ...state, moveTile, rotateTile };
-
-    return (
-        <GameContext.Provider value={ updatedState }>
-            {children}
-        </GameContext.Provider>
-    );
-};
---
+++ frontend/src/hooks/game_state/provider.tsx
@@ -0,0 +1,40 @@
+import { reducer } from "./reducer";
+import { ReactNode, useReducer } from "react";
+import { TileSet, Option, Tile } from "./types";
+import { GameContext, GameState } from "./context";
+
+
+export const GameContextProvider = ({ puzzle, children }: { puzzle: Array<Option<Tile>>, children: ReactNode }) => {
+    const tileset = new TileSet(puzzle);
+    const freeTiles = tileset.iter()
+        .filter(tile =>
+            !puzzle.some(t => t && t.is_equal(tile))
+        );
+
+    const initialState: GameState = {
+        inBoardTiles: puzzle,
+        insertedPositions: [],
+        freeTiles,
+        tileset,
+        moveTile: () => {},
+        rotateTile: () => {},
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const moveTile = (from: number, to: number) => {
+        dispatch({ type: "MOVE_TILE", payload: { from, to } });
+    };
+
+    const rotateTile = (index: number) => {
+        dispatch({ type: "ROTATE_TILE", payload: { index } });
+    };
+
+    const updatedState: GameState = { ...state, moveTile, rotateTile };
+
+    return (
+        <GameContext.Provider value={ updatedState }>
+            {children}
+        </GameContext.Provider>
+    );
+};
